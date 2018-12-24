5a68667c708499cf83cc2af791600b457dc60229
Justin Szeluga <justin.szeluga@gmail.com>
2018-12-23 20:56:44 -0500

    Revert forcing of row limit because it didn't work properly

--- druid/src/main/java/org/apache/calcite/adapter/druid/DruidQuery.java
+++ druid/src/main/java/org/apache/calcite/adapter/druid/DruidQuery.java
@@ -1354,9 +1354,13 @@ public class DruidQuery extends AbstractRelNode implements BindableRel {
         generator.writeStringField("resultFormat", "compactedList");
         if (fetchLimit != null) {
           generator.writeNumberField("limit", fetchLimit);
-        } else {
-          throw new RuntimeException("Row limit must be supplied. Can run out of memory if it is not set");
         }
+
+        //TODO: figure out how to force the query to have a limit
+//        else {
+//          throw new RuntimeException("Row limit must be supplied."
+//              + " Can run out of memory if it is not set");
+//        }
         generator.writeEndObject();
         generator.close();
       } catch (IOException e) {
